e045be7cb8ce4d6210032f6822352aee4e31f72b
Puneeth <157609400+DeKartPlace@users.noreply.github.com>
2024-02-01 00:26:04 +0530

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const { log } = require('console');
 const express=require('express');
+const cors = require('cors');
 const mongoose =require('mongoose');
 const userRouter =require('./routes/user')
 
 const app=express();
 app.use(express.json())
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useNewUrlParser:true})
 .then((result)=>app.listen(process.env.PORT))
